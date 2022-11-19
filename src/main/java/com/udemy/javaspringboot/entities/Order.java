819b1b15ca3c5f7529153c2f91a2bfc27ac50427
Vinícius Castelo <vkstello@gmail.com>
2022-11-19 10:18:54 -0300

    User entity and resource

---
+++ src/main/java/com/udemy/javaspringboot/entities/Order.java
@@ -0,0 +1,10 @@
+package com.udemy.javaspringboot.entities;
+
+import java.util.Date;
+
+public class Order {
+
+    private Long id;
+    private Date moment;
+
+}
---
+++ src/main/java/com/udemy/javaspringboot/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.udemy.javaspringboot.resources;
+
+import com.udemy.javaspringboot.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Vinícius", "vkstello@gmail.com", "(87) 9 9975-4035", "Colore@+1011", null);
+        return ResponseEntity.ok().body(u);
+    }
+
+}
