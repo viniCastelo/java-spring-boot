5b3394f0e7cfa475aff370790a40911aa4ad74b2
Vinícius Castelo <vkstello@gmail.com>
2022-11-20 18:17:15 -0300

    Service layer, component registration

--- src/main/java/com/udemy/javaspringboot/repositories/UserRepository.java
+++ src/main/java/com/udemy/javaspringboot/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.udemy.javaspringboot.repositories;
 
 import com.udemy.javaspringboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
